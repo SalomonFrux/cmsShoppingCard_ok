534eebfe286ea554ab826df7eefa9b372c413c4d
Sam <frux@salomonfrux.onmicrosoft.com>
2019-05-06 00:17:20 +0200

    HttpsGet and HttpPost actions added

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models;
 
 namespace CmsShoppingCart.Areas.Admin.Controllers
 {
@@ -26,11 +27,64 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
 
         }
         // GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
             return View();
 
         }
-       
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Check model state 
+            if(! ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (Sam sam = new Sam()) 
+
+            {
+                // Declare Slug 
+                string Slug;
+                // Initial PageDTO
+                PageDTO dto = new PageDTO();
+                // DTO Title
+                dto.Title = model.Title;
+                // Check for and set Slug if need be
+                if(string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    Slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                  Slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+                // Make sure Tile and Slug are unique
+                if (sam.Pages.Any(x => x.Title == model.Title) || sam.Pages.Any(x=> x.Slug == model.Slug) )
+                {
+                    ModelState.AddModelError(" ", "That title or slug already exist.");
+                    return View(model);
+                }
+                // DTO the rest 
+                dto.Slug = Slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                // Save DTO
+                sam.Pages.Add(dto);
+                sam.SaveChanges();
+            }
+            // Set tempData message
+            TempData["SM"] = "you have successfully added a new page";
+
+            // Redirect 
+            return RedirectToAction("AddPage");
+
+            //return View();
+
+        }
+
     }
-}
+}  
