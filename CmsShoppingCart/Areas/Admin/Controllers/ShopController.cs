c9cb8455ec4e4522d5bf14b75a9e99a4b862620c
Sam <frux@salomonfrux.onmicrosoft.com>
2019-05-19 11:36:10 +0200

    Categorie View Model created and ajax image added

--- CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,23 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop
-        public ActionResult Index()
+        // GET: Admin/Shop/CategoriesPage
+        public ActionResult CategoriesPage()
         {
+            // Declarethe list of View Models
+            List<ShopVM> shopVMs;
+            using (Sam sam = new Sam())
+            {
+                //Int that list 
+                shopVMs = sam.Shops
+                    .ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Select(x => new ShopVM(x))
+                    .ToList();
+
+
+            }
+            // return a view with that list 
             return View();
         }
     }
